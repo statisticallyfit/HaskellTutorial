5ef01bf4a12aa404c5925d3659ac3b792a25ffbe
BabitzdaBrave <amg.vintila@gmail.com>
2016-05-22 00:21:57 -0700

    save Functor

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter16_Functor/Functor.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter16_Functor/Functor.hs
@@ -49,18 +49,122 @@ main = do
 
 data FixMePls a = FixMe | Pls a deriving (Eq, Show)
 
+
+{-
+HELP don't understand this error
+The first argument of `Functor' should have kind `* -> *',
+      but `FixMePls a' has kind `*'
+    In the instance declaration for `Functor (FixMePls a)'
+
+    when type is: instance Functor (FixMePls a) where
+-}
+
+
 instance Functor FixMePls where
-    fmap :: Functor f => (a -> b) ->   f a ->   f b
+    --fmap :: Functor f => (a -> b) ->   f a ->   f b -- HELP why error when uncommented?
     -- note meaning:         f       (Pls a) = Pls (f a)
     fmap _ FixMe = FixMe
     fmap f (Pls a) = Pls (f a)
 
-    {-
+    {- HELP - f a is not Pls a since f is (a -> b) so help
         While f is used in the type of fmap to represent the Functor, by convention,
         it is also conventionally used in function deﬁnitions to name
         an argument that is itself a function. Don’t let the names fool you
         into thinking the f in our FixMePls instance is the same f as in the
         Functor typeclass deﬁnition. note: but it represents the same function.
     -}
+{-uncover
 main = do
     print $ fmap (+1) (Pls 1)
+-}
+
+
+
+
+
+
+
+
+-- 16.5 FUNCTOR LAWS ----------------------------------------------------------------
+
+-- NOTE: the function (f) must be any function with a type that is an instance
+-- of Functor
+
+
+-- note: first law: identity
+-- fmap id anything = anything
+
+-- note: second law: composition:
+-- fmap (f . g)  == fmap f . fmap g
+
+{-uncover
+main = do --------------------------------- identity law
+    print $ fmap id "Hi Julie"
+    print $ fmap id (Just 1)
+    --------------------------------------- composition law
+    print $ fmap ((+1) . (*2)) [1..5]
+    print $ fmap (+1) . fmap (*2) $ [1..5]
+-}
+
+
+
+
+
+
+
+-- 16.6 (TESTING FUNCTOR LAWS) ----------------------------------------------------
+
+data WhoCares a = ItDoesnt
+                | Matter a
+                | WhatThisIsCalled
+                deriving (Eq, Show)
+
+
+-- NOTE this is a law abiding Functor instance (identity)
+instance Functor WhoCares where
+    fmap _ ItDoesnt = ItDoesnt
+    fmap _ WhatThisIsCalled = WhatThisIsCalled
+    fmap f (Matter a) = Matter (f a)
+
+
+
+-- NOTE THIS IS NOT law abiding functor instance (identity)
+{-
+instance Functor WhoCares where
+    fmap _ ItDoesnt = WhatThisIsCalled
+    fmap f WhatThisIsCalled = ItDoesnt
+    fmap f (Matter a) = Matter (f a)
+-}
+
+{-uncover HELP why doesn't this work? Need to do it directly in ghci
+main = do
+    print $ fmap id ItDoesnt
+    print $ fmap id WhatThisIsCalled
+    print $ fmap (+10) (Matter 10)
+-}
+
+
+
+
+
+
+
+data CountingBad a = Heisenberg Int a deriving (Eq, Show)
+
+-- NOTE this is NOT law abiding (composition)
+instance Functor CountingBad where
+    fmap f (Heisenberg n a) = Heisenberg (n+1) (f a)
+
+
+-- NOTE this IS law abiding (composition)
+
+
+
+
+oneWhoKnocks = Heisenberg 0 "Uncle"
+f = (++ " Jesse")
+g = (++ " lol")
+
+main = do
+    print $ fmap (f . g) oneWhoKnocks
+    print $ fmap f . fmap g $ oneWhoKnocks
