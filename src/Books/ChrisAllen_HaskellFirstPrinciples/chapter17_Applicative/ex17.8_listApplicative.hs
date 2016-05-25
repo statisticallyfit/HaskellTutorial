91874f195323916814f888f593ce7f7d4d5c0d6d
BabitzdaBrave <amg.vintila@gmail.com>
2016-05-25 07:20:10 -0700

    save 5:20

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter17_Applicative/ex17.8_listApplicative.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter17_Applicative/ex17.8_listApplicative.hs
@@ -7,16 +7,35 @@ import Test.QuickCheck.Classes
 data List a = Nil | Cons a (List a) deriving (Eq, Show)
 
 
-instance Functor List where
-    fmap _ Nil = Nil
-    fmap f (Cons x xs) = (Cons f x) (fmap f xs)
 
+
+append :: List a -> List a -> List a
+append Nil ys = ys
+append (Cons x xs) ys = Cons x $ xs `append` ys
+
+fold :: (a -> b -> b) -> b -> List a -> b
+fold _ seed Nil = seed
+fold f seed (Cons h t) = f h (fold f seed t)
+-- h = head, t = tail
+
+concat' :: List (List a) -> List a
+concat' lol = fold append Nil lol
+
+flatMap :: (a -> List b) -> List a -> List b
+flatMap f as = concat' $ fmap f as
+
+
+{-
 instance Monoid (List a) where
     mempty = Nil
     mappend a Nil = a
     mappend Nil a = a
     mappend (Cons x xs) ys = Cons x $ xs `mappend` ys
-
+-}
+instance Functor List where
+    fmap _ Nil = Nil
+    fmap f (Cons x xs) = (Cons f x) (fmap f xs)
+{-
 instance Applicative List where
     pure x = Cons x Nil
     (<*>) Nil _ = Nil
@@ -41,3 +60,4 @@ main = do
     quickBatch $ applicative ( [("b", "w", 1)] :: [(String, String, Int)])
     quickBatch $ applicative ((Just ("b", "w", 1)) :: (Maybe (String, String, Int)))
     quickBatch $ applicative ((Cons ("b", "w", 1) Nil) :: (List (String, String, Int)))
+-}
