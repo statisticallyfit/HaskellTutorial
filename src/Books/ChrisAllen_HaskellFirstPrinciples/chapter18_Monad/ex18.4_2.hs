9544741e7606598878c4a6346427dd3a5fc7222a
BabitzdaBrave <amg.vintila@gmail.com>
2016-05-26 23:25:02 -0700

    save

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter18_Monad/ex18.4_2.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter18_Monad/ex18.4_2.hs
@@ -1,6 +1,7 @@
 import Data.Monoid (Monoid, (<>))
 import Control.Applicative
-import Test.QuickCheck (Arbitrary, arbitrary, elements)
+import Control.Monad (join)
+import Test.QuickCheck (Arbitrary, arbitrary, elements, frequency)
 import Test.QuickCheck.Checkers (quickBatch, eq, (=-=), EqProp)
 import Test.QuickCheck.Classes (applicative)
 
@@ -25,15 +26,18 @@ instance Applicative (Either' e) where
 
 instance Monad (Either' e) where
     return = pure
-    (>>=) _ (Left' e) = Left' e
+    --(>>=) _ (Left' e) = Left' e
     (>>=) (Left' e) _ = Left' e
-    (>>=) (Right' f) (Right' a) = join $ fmap (f a)
+    (>>=) (Right' a) f = f a
+    --(>>=) (Right' f) (Right' a) = join $ fmap (f a)
 
 
 ------------------------------------------------------------------------------
 instance (Arbitrary e, Arbitrary a) => Arbitrary (Either' e a)  where
-    arbitrary = frequency [(1, Left' <$> arbitrary),
-                           (3, Right' <$> arbitrary)]
+    arbitrary = do
+        e <- arbitrary
+        a <- arbitrary
+        elements [Left' e, Right' a]
 
 instance (Eq e, Eq a) => EqProp (Either' e a) where
     (=-=) = eq
