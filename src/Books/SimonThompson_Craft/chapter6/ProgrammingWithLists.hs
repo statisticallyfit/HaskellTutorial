fd0235c27cf4492d375bad9c46f64645175a7e90
BabitzdaBrave <amg.vintila@gmail.com>
2016-06-05 04:21:52 -0700

    save on exercise rotate 90

---
+++ src/Books/SimonThompson_Craft/chapter6/ProgrammingWithLists.hs
@@ -0,0 +1,91 @@
+import Test.QuickCheck
+
+-- 6.4 PICTURE EXAMPLE --------------------------------------------------------------------
+
+type Picture = [[Char]]
+
+-- note flipping picture horizontally, just reverse order of lines in picture.
+flipH :: Picture -> Picture
+flipH = reverse
+
+
+-- note place one picture above another, join two lists of lines together
+above :: Picture -> Picture -> Picture
+above = (++)
+
+
+-- note flip picture in a vertical mirror
+flipV :: Picture -> Picture
+flipV pic = [reverse line | line <- pic]
+
+
+-- note place two pictures side by side, by joining up their corresponding lines.
+beside :: Picture -> Picture -> Picture
+beside picL picR = [lineL ++ lineR | (lineL, lineR) <- zip picL picR]
+
+
+-- note inverting the color of pictures
+invertChar :: Char -> Char
+invertChar ch = if ch == ' ' then '#' else ' '
+
+invertLine :: [Char] -> [Char]
+invertLine line = [invertChar ch | ch <- line]
+
+-- note now apply inversion to all lines in the picture
+invertColour :: Picture -> Picture
+invertColour pic = [invertLine line | line <- pic]
+
+
+
+
+onSeperateLines :: Picture -> String
+onSeperateLines stringList = concat [word ++ "\n" | word <- stringList]
+
+draw :: Picture -> IO()
+draw pic = putStr $ onSeperateLines pic
+
+
+p1 = ["##########", "######....", "###.......", "##........", "#........."]
+p2 = ["##########", "###....###", "###....###", "###....###", "##########"]
+p = ["...####...", "...#..#...", "...####...", "...#......", "...#......"]
+b = ["...####...", "...#...#..", "...####...", "...#...#..", "...####..."]
+l = ["..#.......", "..#.......", "..#.......", "..#.......", "..######.."]
+n = ["..#....#..", "..#.#..#..", "..#..#.#..", "..#...##..", "..#....#.."]
+
+
+
+{-
+p1 = ["##########", "######    ", "###       ", "##        ", "#         "]
+p2 = ["##########", "###    ###", "###    ###", "###    ###", "##########"]
+p3 = ["   ####   ", "   #  #   ", "   ####   ", "   #      ", "   #      "]
+-}
+
+
+
+
+-- NOTE Testing
+
+prop_AboveFlipV :: Picture -> Picture -> Bool
+prop_AboveFlipV p1 p2 = flipV (p1 `above` p2) == (flipV p1) `above` (flipV p2)
+
+-- help fails because quickcheck generates random data with lists not of same length ...
+prop_AboveFlipH :: Picture -> Picture -> Bool
+prop_AboveFlipH p1 p2 = flipH (p1 `above` p2) == (flipH p1) `beside` (flipH p2)
+
+
+
+-- note ==> means implies and property following ==> is only checked when condition before
+-- it is true.
+-- HELP doesn't work, what does this test mean ?
+propAboveBeside3Correct :: Picture -> Picture -> Property
+propAboveBeside3Correct w e = (rectangular w $$ rectangular e $$ height w == height e)
+    ==>
+        (w `beside` e) `above` (w `beside` e)
+            ==
+        (w `above` w) `beside` (e `above` e)
+
+
+main = do
+    quickCheck prop_AboveFlipV
+    --quickCheck prop_AboveFlipH
+    -- quickCheck propAboveBeside3Correct-- HELP
---
+++ src/Books/SimonThompson_Craft/chapter6/exercise6.4_6.5_6.6.hs
@@ -0,0 +1,46 @@
+type Picture = [[Char]]
+
+
+superimposeChar :: Char -> Char -> Char
+superimposeChar '.' '.' = '.'
+superimposeChar _ _ = '#'
+
+
+superimposeLine :: String -> String -> String
+superimposeLine l1 l2 = [superimposeChar c1 c2 | (c1, c2) <- zip l1 l2]
+
+
+superimpose :: Picture -> Picture -> Picture
+superimpose p1 p2 = [superimposeLine l1 l2 | (l1, l2) <- zip p1 p2{-l1 <- p1, l2 <- p2-}]
+-- note the commented out part is basically a foreach - best to use zip. So eitherwise
+-- it would be for each l1, do the same l2, ...
+
+
+{-
+onSeperateLines :: Picture -> String
+onSeperateLines stringList = concat [word ++ "\n" | word <- stringList]
+-}
+
+draw :: Picture -> IO()
+draw pic = putStr $ onSeperateLines pic
+           where onSeperateLines pic = [letter | picLine <- pic, letter <- picLine ++ "\n"]
+
+
+p1 = ["##########", "######....", "###.......", "##........", "#........."]
+p2 = ["##########", "###....###", "###....###", "###....###", "##########"]
+p  = ["...####...", "...#..#...", "...####...", "...#......", "...#......"]
+b  = ["...####...", "...#...#..", "...####...", "...#...#..", "...####..."]
+l  = ["..#.......", "..#.......", "..#.......", "..#.......", "..######.."]
+n  = ["..#....#..", "..#.#..#..", "..#..#.#..", "..#...##..", "..#....#.."]
+
+
+main = do
+    print $ superimposeChar '.' '.'
+    print $ superimposeChar '.' '#'
+    print $ superimposeChar '#' '.'
+    print $ superimposeChar '#' '#'
+    putStrLn "" --------------------------
+    print $ superimposeLine ".##." ".#.#"
+    putStrLn "" --------------------------
+    draw $ superimpose n p1
+    draw $ superimpose p1 n
