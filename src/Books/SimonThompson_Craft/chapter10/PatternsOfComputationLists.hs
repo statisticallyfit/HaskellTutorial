6d6e3d8d2afbf0063922fa6699b57a12f1a8297c
BabitzdaBrave <amg.vintila@gmail.com>
2016-06-10 10:25:27 -0700

    done chapter 9, started chapter 10 skimmed mostly chapter 9 can come back later

---
+++ src/Books/SimonThompson_Craft/chapter10/PatternsOfComputationLists.hs
@@ -0,0 +1,28 @@
+
+
+-- MAPPING
+-- taking second element of each pair in list of pairs
+-- formatting each (n,p) pair in a list
+
+ -- FILTERING
+ -- getting digits out of an address
+
+-- FOLDING (combining items)
+-- folding a function (+) over a list with seed (0)
+-- foling (++) over a list of lists to concatenate
+-- folding (&&) over a list of booleans to take their conjunction.
+-- folding max over a list to give maximum.
+
+
+-- BREAKING UP LISTS
+-- getWord, dropSpace...
+
+
+
+-- COMBINATIONS OF ALL THESE
+
+
+
+ -- 10.2 HIGHER ORDER FUNCTIONS --------------------------------------------------------
+
+-- 1 MAP 
--- src/Books/SimonThompson_Craft/chapter9/Proofs.hs
+++ src/Books/SimonThompson_Craft/chapter9/Proofs.hs
@@ -53,3 +53,161 @@ doubleAll (z:zs) = 2*z : doubleAll zs
 
 propSumDoubleAll :: [Integer] -> Bool
 propSumDoubleAll xs = sum (doubleAll xs) == (2 * sum xs)
+
+
+
+{-
+NOTE
+
+1. base case
+
+LEFT:
+sum (doubleAll [])
+= sum []
+= 0
+
+RIGHT:
+2 * sum []
+= 2 * 0
+= 0
+
+
+
+2. induction step
+
+induction hypothesis: sum (doubleAll xs) == 2 * sum xs
+
+LEFT:
+sum (doubleAll (x:xs))
+= sum (2*x : doubleAll xs)
+= 2*x + sum (doubleAll xs)
+
+RIGHT:
+2 * sum (x:xs)
+= 2 * (x + sum xs)
+= 2*x + 2 * sum xs
+
+
+
+LAST step to equate the two:
+
+sum (doubleAll (x:xs))
+= sum (2*x : doubleAll xs)
+= 2*x + sum (doubleAll xs)
+= 2*x + 2 * sum xs
+-}
+
+
+
+
+
+{-
+NOTE how to prove recursive functions:
+
+1. state as quickcheck property
+2. state goal of induction and sub goals (base) (hyp) => (ind)
+3. use arithmetic and functin definition to simplify sub goals.
+4. label each step with its justification.
+-}
+
+-- EXAMPLE 1 ---> length and (++)
+
+propLengthPlusPlus :: [a] -> [a] -> Bool
+propLengthPlusPlus xs ys = length (xs ++ ys) == length xs + length ys
+
+{-
+Use the fact that (++) is defined as:
+[]     ++ zs = zs
+(w:ws) ++ zs = w : (ws ++ zs)
+
+
+1. BASE CASE: ------------------------------------------------------------------------
+length ([] ++ ys) = length [] + length ys
+
+
+LEFT
+length ([] ++ ys)
+= length ys
+
+RIGHT
+length [] + length ys
+= 0 + length ys
+= length ys
+
+
+2. INDUCTION HYPOTHESIS: -------------------------------------------------------------
+length (xs ++ ys) = length xs + length ys
+
+(assume)
+
+3. INDUCTION STEP --------------------------------------------------------------------
+length ((x:xs) ++ ys) = length (x:xs) + length ys
+
+(prove)
+
+LEFT
+length ((x:xs) ++ ys)
+= length (x:(xs ++ ys))   --- applied definition of (++)
+= 1 + length (xs ++ ys)
+= 1 + length xs + length ys
+
+
+RIGHT
+length (x:xs) + length ys
+= 1 + length xs + length ys
+
+-}
+
+
+
+-- EXAMPLE 2 ----> reverse and ++
+-- propReversePlusPlus       :: Eq a => [a] -> [a] -> Bool
+-- note use non-generic type because otherwise all elements are the same.
+propReversePlusPlus       :: [Integer] -> [Integer] -> Bool
+propReversePlusPlus xs ys = reverse (xs ++ ys) == reverse ys ++ reverse xs
+
+{-
+
+Use the fact that reverse is defined as:
+reverse [] = []
+reverse (z:zs) = reverse zs ++ [z]
+
+
+1. BASE -----------------------------------------------------------------------------
+
+reverse ([] ++ ys) = reverse ys ++ reverse []
+
+LEFT
+reverse ([] ++ ys)
+= reverse ys
+
+RIGHT
+reverse ys ++ reverse []
+= reverse ys ++ []  -- NOTE must prove this is an identity operation
+
+
+
+2. HYP ------------------------------------------------------------------------------
+
+reverse (xs ++ ys) = reverse ys ++ reverse xs
+
+(assume)
+
+3. IND ------------------------------------------------------------------------------
+reverse ((x:xs) ++ ys) = reverse ys ++ reverse (x:xs)
+(prove)
+
+
+LEFT
+reverse ((x:xs) ++ ys)
+= reverse (x:(xs ++ ys))
+= reverse (xs ++ ys) ++ [x]
+= (reverse ys ++ reverse xs) ++ [x]
+
+RIGHT
+reverse ys ++ reverse (x:xs)
+= reverse ys ++ (reverse xs ++ [x])       NOTE must prove (++) is associative
+                                          NOTE xs ++ (ys ++ zs) = (xs ++ ys) ++ zs
+
+
+-}
