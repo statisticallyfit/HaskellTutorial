7675873868a6012c8d441245819aee33a105eb5d
BabitzdaBrave <amg.vintila@gmail.com>
2016-06-11 10:00:29 -0700

    save on ex15

--- src/Books/SimonThompson_Craft/chapter10/exercise10.15.hs
+++ src/Books/SimonThompson_Craft/chapter10/exercise10.15.hs
@@ -1,7 +1,7 @@
 import Prelude hiding (unzip, last, init)
 
 
-
+-- 1 unzip
 unzip' :: [(a, b)] -> ([a], [b])
 unzip' [] = ([], [])
 unzip' xs = (foldr first [] xs, foldr second [] xs)
@@ -9,9 +9,19 @@ unzip' xs = (foldr first [] xs, foldr second [] xs)
            second pair zs = [snd pair] ++ zs
 
 
-
+-- HElP
 unzip :: [(a, b)] -> ([a], [b])
 unzip xs = foldr (\(a,b) (as,bs) -> (a:as, b:bs)) ([], []) xs
 
 -- note unzip xs = foldr (\(a,b) ~(as,bs) -> (a:as, b:bs)) ([], []) xs
--- the tilde means enforce lazy evaluation. 
+-- the tilde means enforce lazy evaluation.
+
+
+
+
+-- 2 last
+
+init'        :: [a] -> [a]
+init' [x]    = []
+init' (x:xs) = x : init' xs
+
