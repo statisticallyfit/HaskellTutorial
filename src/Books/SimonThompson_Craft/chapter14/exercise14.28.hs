7d997d1ded8b359372bfc609066ec6e88764206c
BabitzdaBrave <amg.vintila@gmail.com>
2016-06-20 23:28:55 -0700

    still non exhaustive transform

--- src/Books/SimonThompson_Craft/chapter14/exercise14.28.hs
+++ src/Books/SimonThompson_Craft/chapter14/exercise14.28.hs
@@ -10,10 +10,22 @@ data Edit = Change Char
 -- note finds lowest cost sequence of edits to take us from one string to another.
 -- in general case, if first two chars of strnisg are equal, then Copy. Otherwise,
 -- try all possibilities and choose the best of them.
+-- HELP how to make this changed function shorter? 
 transform :: String -> String -> [Edit]
 transform [] [] = []
 transform xs [] = [Kill] -- note to turn xs -> [] just kill it
 transform [] ys = map Insert ys  -- note to turn [] -> ys insert ys.
+transform [x] [y]
+    | x == y = Copy : []
+    | otherwise = best [Delete   : transform [] [y],
+                        Insert y : transform [x] [],
+                        Change y : transform [] []]
+transform (a:b:[]) (d:e:[])
+    | a == d = Copy : transform [b] [e]
+    | a == e && b == d = Swap : []
+    | otherwise = best [Delete   : transform [b] [d,e],
+                        Insert d : transform [a,b] [e],
+                        Change d : transform [b] [e]]
 transform (a:b:cs) (d:e:fs) -- (x:xs) (y:ys)
     | a == d = Copy : transform xs ys
     | a == e && b == d = Swap : transform cs fs
@@ -43,5 +55,6 @@ edit [] xs = xs
 edit (Insert c : rest) (x:xs) = c : edit rest (x:xs)
 edit (Change c : rest) (x:xs) = c : edit rest xs
 edit (Copy : rest) (x:xs) = x : edit rest xs
+edit (Swap : rest) (a:b:cs) = b: a : edit rest cs
 edit (Delete : rest) (x:xs) = edit rest xs
 edit (Kill : rest) xs = []
