41910025bcf165b0c271a12a277fd8588a1bf59c
BabitzdaBrave <amg.vintila@gmail.com>
2016-06-27 01:13:14 -0700

    finished implementation of QueueState and going to start from book on Server

--- src/Books/SimonThompson_Craft/chapter16/AbstractDataTypes.hs
+++ src/Books/SimonThompson_Craft/chapter16/AbstractDataTypes.hs
@@ -189,7 +189,7 @@ qs2 = QueueSplit ["let's", "go", "fly", "a", "kite"]
 
 
 
--- 16.5 SIMULATION ----------------------------------------------------------------------
+-- 16.5,6 SIMULATION ----------------------------------------------------------------------
 
 {-
 -- EXAMPLE  get input of customer arrivals (queues) and give output their
@@ -211,9 +211,77 @@ At a given time
 
     data OutputMsg = None | Discharge Arrival Wait Service
     type Wait = Integer
+
+
+3. Two operations in a queue: add item and process quque by one-minute step.
+This gives one minute's further processing to item at head of queue.
+Two outcomes are possible: item might have its processing completed, generates an Outmess,
+or further processing is needed.
+
+
+
+4. ServerState models serving more than one quque. System receives one Inmess per minute.
+Three principal operations on a server (collection of queues):
+    1. add inmess to one of the queues.
+    2. processing step
+    3. combine a server step with allocation of Inmess to shortest queue in the server.
+
+    Three other operations are necessary:
+    1. make a starting server with appropriate number of empty queues.
+    2. identify num queues in server
+    3. identify shrotest queue in server.
 -}
 
 
+-- => no one arrives (No)
+-- => someone arrives (Yes (arrival time of customer) (time to serve them))
+type Arrival = Integer
+type Service = Integer
+data Inmess = No | Yes Arrival Service deriving (Eq, Show)
 
 
+-- =>  no one leaves (None)
+-- => or a person is discharged (Discharge) which takes the time the customer
+-- waited and time of their arrival and time it took to serve them.
+type Wait = Integer
+data Outmess = None | Discharge Arrival Wait Service deriving (Eq, Show)
 
+-- Time = current time
+-- Service = service time so far for the item being curently processed.
+-- [Inmess] = the actual queue.
+data QueueState = QS Time Service [Inmess] deriving (Eq, Show)
+
+
+addMessage :: Inmess -> QueueState -> QueueState
+addMessage im (QS time serv q) = QS time serv (q ++ [im])
+
+-- note If no inmess queue to process, incr time by one  and return empty in/outmesses.
+-- If serving time so far is less than required (servTime) then processing is
+-- not compelte. Therefore add one to current time and to service time so far.
+-- Otherwise the new state of the queue has time advanced by one, head removed, processing
+-- time is set to 0. Output message is produced: waiting time = current time subtracted
+-- by both service and arrival times.
+-- We incrememnt currTime in both cases because it took 1 minute to conduct this
+-- queueStep operation.
+-- note the "time" variables here are times (amounts) not actual clock point in time
+-- such as 5:45 pm.
+-- note case of messages "No" are filtered out by server.
+queueStep :: QueueState -> (QueueState, [Outmess])
+queueStep (QS currTime servTimeSoFar []) = (QS (currTime+1) servTimeSoFar [], [])
+queueStep (QS currTime servTimeSoFar inQ@(Yes arrTime servTime : inRest))
+    | servTimeSoFar < servTime
+        = (QS (currTime+1) (servTimeSoFar+1) inQ, [])
+    | otherwise
+        = (QS (currTime+1) 0 inRest
+                [Discharge arrTime waitTime servTime]) ------ this is the outmesss
+    where waitTime = currTime - servTime - arrTime
+
+
+queueStart :: QueueState
+queueStart = QS 0 0 []
+
+queueLength :: QueueState -> Int
+queueLength (QS _ _ q) = length q
+
+queueEmpty :: QueueState -> Bool
+queueEmpty (QS _ _ ) = q == []
