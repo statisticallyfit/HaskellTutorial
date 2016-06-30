6f3265d3fd7898787b262c50c2f02e62c7f250a1
BabitzdaBrave <amg.vintila@gmail.com>
2016-06-30 10:39:10 -0700

    trying to do exercises

--- src/Books/SimonThompson_Craft/chapter16/Graph.hs
+++ src/Books/SimonThompson_Craft/chapter16/Graph.hs
@@ -157,6 +157,9 @@ depthList rel (node : restNode) visited
 graph1 :: Graph Integer
 graph1 = Set [(1,2), (1,3), (3,2), (3,4), (4,2), (2,4),(3,6),(3,9)]
 
+graph2 :: Graph Integer
+graph2 = graph1 `union` Set [(4,3)]
+
 
 r1 :: Relation Integer
 r1 = Set [(1,2),(2,3),(3,4)]
