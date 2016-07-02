6efb3f7391f8bcbd388d65f4a8cb2f5e443dab39
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-02 02:06:18 -0700

    changed name to isHamPrime from before was isEither1235

--- src/Books/SimonThompson_Craft/chapter17/exercises/exercise17.24.hs
+++ src/Books/SimonThompson_Craft/chapter17/exercises/exercise17.24.hs
@@ -1,16 +1,19 @@
 
 
-factors :: Int -> [Int]
-factors n = filter (\x -> mod n x == 0) upTo
+makeFactors :: Int -> [Int]
+makeFactors n = filter (\x -> mod n x == 0) upTo
     where upTo = [1 .. n]
 
 
 
 hamming :: [Int]
 hamming = filter (map isHamming) factorsOfEachNum
-    where factorsOfEachNum = map factors [1..]
-          isEither235 n = n == 2 || n == 3 || n == 5
-          isHamming facList = filter isEither235 facList
+    where factorLists = map makeFactors [1..]
+          onlyHamPrimes fs = filter isHamPrime fs
+          isHamPrime n = n == 1 || n == 2 || n == 3 || n == 5
+          hasFactorsOtherThanHamPrimes fs = length fs /= (length onlyHamPrimes fs)
+          isHamming factorLists = filter isHamPrime factorLists
+
 
 -- main code: filter (\n -> isHamming (factors n)) nums
 -- isHamming facs = (length $ filter isEither235 facs) /= 0
