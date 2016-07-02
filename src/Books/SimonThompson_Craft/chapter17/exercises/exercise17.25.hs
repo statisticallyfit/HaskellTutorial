253896796c881ca28824d43ab8779e1912abc6da
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-02 09:27:19 -0700

    understand better the given listSums solution

--- src/Books/SimonThompson_Craft/chapter17/exercises/exercise17.25.hs
+++ src/Books/SimonThompson_Craft/chapter17/exercises/exercise17.25.hs
@@ -1,6 +1,6 @@
 import Test.QuickCheck
 
--- NOTE I think I used memoization! 
+-- NOTE I think I used memoization!
 runningSums :: [Int] -> [Int]
 runningSums numbers = runner numbers [0]  -- starting with 0 as first element.
     where runner [] cumSums = cumSums
@@ -12,3 +12,11 @@ testRunningSumsLength xs = (length $ runningSums xs) == (length xs + 1)
 
 testRunningSumsLastElementIsTotal :: [Int] -> Bool
 testRunningSumsLastElementIsTotal xs = last (runningSums xs) == (sum xs)
+
+
+
+-- help todo understand better how this works. page 472
+-- help how do the sums get to the front of the list? in front of zipwith? 
+listSums :: [Integer] -> [Integer]
+listSums iList = out
+    where out = 0 : zipWith (+) iList out
