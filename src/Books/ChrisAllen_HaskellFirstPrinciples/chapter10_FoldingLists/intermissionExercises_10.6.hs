cbd59d0f02f4ed01331ebef515e8de262b47d085
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-04 07:54:21 -0700

    made filterDBDate and number

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter10_FoldingLists/intermissionExercises_10.6.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter10_FoldingLists/intermissionExercises_10.6.hs
@@ -12,8 +12,22 @@ theDataBase = [DbDate (UTCTime
                             (secondsToDiffTime 34123)),
                DbNumber 9001,
                DbString "Hello, world!",
+               DbNumber 187234,
                DbDate (UTCTime
-                            (fromGregorian 1921 5 1),
-                            (secondsToDiffTime 34123))
-               ]
+                            (fromGregorian 1921 5 1)
+                            (secondsToDiffTime 34123))]
 
+--- 1
+
+-- note the otherConstructor will go again into the accumulation and will be evaluated
+-- by function (f).
+filterDbDate :: [DatabaseItem] -> [UTCTime]
+filterDbDate dBase = foldr f [] dBase
+    where f (DbDate utc) otherConst = utc : otherConst
+          f _ otherConst = otherConst
+
+--- 2
+filterDbNumber :: [DatabaseItem] -> [Integer]
+filterDbNumber dBase = foldr f [] dBase
+    where f (DbNumber n) otherConst = n : otherConst
+          f _ otherConst = otherConst
