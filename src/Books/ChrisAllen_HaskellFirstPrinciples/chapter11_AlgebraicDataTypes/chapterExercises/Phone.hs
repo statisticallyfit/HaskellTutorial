fc72d9bf5bd518273bd6ce67b5cf8939023ceba1
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-07 04:26:26 -0700

    made phone pad now make functions

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter11_AlgebraicDataTypes/chapterExercises/Phone.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter11_AlgebraicDataTypes/chapterExercises/Phone.hs
@@ -1,5 +1,6 @@
 module Phone where
 
+import Data.Char
 
 {-
 IMPORTANT RULES:
@@ -9,15 +10,39 @@ IMPORTANT RULES:
 more presses than digits in button get you back around to letters on button.
 -}
 
-data Phone = Phone
+type Digit = Char
+type Symbol = Char
+type Presses = Int -- >= 1
+data Button = Button Digit Symbol deriving (Eq, Show)
+data Phone = PhonePad [Button] deriving (Eq, Show)
+
+
+
+
+
+
 
 
 text :: [String]
 text =
     ["The night sky is littered with stars.",
+     "Sparkling water",
      "Swallows descend from their nightly perches and race in the grey dawn",
      "Mountains tower, their craggy faces rough and weather-hewn",
+     "Twittering birds",
+     "Soaring eagles",
      "Morning dew settles on lilac bushes, and fog rises off the lake in golden splendor",
-     "Wind scours, molds, shapes, and devours, leaving traced frozen lava in its wake."]
+     "Volcanic ash",
+     "Wind scours, molds, and shapes, leaving traced frozen lava in its wake.",
+     "Glowing crystals"]
 
 
+
+phone :: IO()
+phone = putStr $ break ++ row123 ++ break ++ row456 ++ break ++
+                   row789 ++ break ++ rowOp ++ break
+    where break  = "-------------------------------------\n"
+          row123 = "|   1       |   2 ABC   |   3 DEF   |\n"
+          row456 = "|   4 GHI   |   5 JKL   |   6 MNO   |\n"
+          row789 = "|   7 PQRS  |   8 TUV   |   9 WXYZ  |\n"
+          rowOp  = "|    * ^    |   0 + _   |    # .,   |\n"
