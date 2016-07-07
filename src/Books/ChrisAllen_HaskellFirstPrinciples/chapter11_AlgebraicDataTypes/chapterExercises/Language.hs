b26d605fac7a115d395dab534056a9a43bd1794d
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-06 23:04:55 -0700

    done subsequence and testing

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter11_AlgebraicDataTypes/chapterExercises/Language.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter11_AlgebraicDataTypes/chapterExercises/Language.hs
@@ -1,5 +1,44 @@
+import Data.List hiding (isSubsequenceOf)
+import Data.Maybe
+import Test.QuickCheck
 
 
+
+-- note tests if each element is greater than the other - doesn't tolerate duplicates.
+sortedStrict :: Ord a => [a] -> Bool
+sortedStrict xs = xs == sort xs && (length xs == length (nub xs))
+
+--- note tests whether the sub portion is contained in its order inside whole.
+-- IT can be spread out so there are other elements from whole interspersed, but it
+-- just cannot be in a different order than sub itself.
 
 isSubsequenceOf :: Eq a => [a] -> [a] -> Bool
-isSubsequenceOf
+isSubsequenceOf sub whole
+    | isSubPresent = isSubSameOrder
+    | otherwise = False
+    where isSubPresent = and $ map ((flip elem) whole) sub
+          justLocs = map ((flip elemIndex) whole) sub
+          locs = map fromJust (justLocs)
+          isSubSameOrder = sortedStrict locs
+
+
+
+
+--- TESTING ------------------------------------------------------------------------------
+
+--- testing that elements are strictly sorted
+testStrictSort :: [Int] -> Bool
+testStrictSort xs = (noDups xs && sorted xs) == sortedStrict xs
+    where noDups xs = nub xs == xs
+          sorted xs = sort xs == xs
+
+--- testing that reverse word is not subsequence of word itself.
+testSubReverse :: String -> Bool
+testSubReverse word = if (length word == 0 || length word == 1) then True else mainOccs
+    where mainOccs = isSubsequenceOf word (reverse word) == False
+
+
+--- testing if is subsequence then automatically each letter of sub is inside the whole.
+testSubContainLetter :: String -> String -> Bool
+testSubContainLetter sub whole = (isSubsequenceOf sub whole) == containedLetters
+    where containedLetters = and $ map ((flip elem) whole) sub
