d3663bebe957329707963a2e72f46d5c23cc87e6
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-07 11:50:49 -0700

    done dict words going to start hangman chapter 13

---
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter13_BuildingProjects/Hangman.hs
@@ -0,0 +1,9 @@
+module Hangman where
+
+import Control.Monad (forever)
+import Data.Char (toLower)
+import Data.Maybe (isJust)
+import Data.List (intersperse)
+import System.Exit (exitSuccess)
+import System.Random (randomRIO)
+
