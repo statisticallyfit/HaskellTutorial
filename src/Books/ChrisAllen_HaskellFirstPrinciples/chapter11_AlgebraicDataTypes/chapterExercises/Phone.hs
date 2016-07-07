83630d7a475bd8cb2ccaa78b13f873c0b11e1d02
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-07 02:10:43 -0700

    save

---
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter11_AlgebraicDataTypes/chapterExercises/Phone.hs
@@ -0,0 +1,12 @@
+module Phone where
+
+
+{-
+IMPORTANT RULES:
+1.(*) is capitalization of letter
+2. (0) is space bar
+3. to get a digit, push the buton as many times as digit's location. Do wraparound so
+more presses than digits in button get you back around to letters on button.
+-}
+
+data Phone = Phone 
