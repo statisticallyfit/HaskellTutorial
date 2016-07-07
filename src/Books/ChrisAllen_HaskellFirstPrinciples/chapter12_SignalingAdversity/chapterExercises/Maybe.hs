dbe0fc185d13eeba2e74ac28daf3fec6f05f8d00
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-07 07:44:13 -0700

    did up to num 5

---
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter12_SignalingAdversity/chapterExercises/Maybe.hs
@@ -0,0 +1,47 @@
+
+
+--- 1
+isJust :: Maybe a -> Bool
+isJust (Just _) = True
+isJust _ = False
+
+isNothing :: Maybe a -> Bool
+isNothing Nothing = True
+isNothing _ = False
+
+
+
+--- 2
+
+-- >> maybe 0 (+1) Nothing    --> 0
+-- >> maybe 55 (+1) (Just 13) --> 14
+mayybee :: b -> (a -> b) -> Maybe a -> b
+mayybee defaultVal f (Just x) = f x
+mayybee defaultVal _ Nothing = defaultVal
+
+
+
+--- 3
+
+-- >> fromMaybe 0 Nothing  ---> 0
+-- >> fromMaybe 0 (Just 1) ---> 1
+fromMaybe :: a -> Maybe a -> a
+fromMaybe defaultVal (Just x) = x
+fromMaybe defaultVal Nothing = defaultVal
+
+
+
+--- 4
+
+listToMaybe :: [a] -> Maybe a
+listToMaybe [] = Nothing
+listToMaybe xs = Just $ head xs
+
+
+--- 5
+maybeToList :: Maybe a -> [a]
+maybeToList Nothing = []
+maybeToList (Just x) = [x]
+
+
+
