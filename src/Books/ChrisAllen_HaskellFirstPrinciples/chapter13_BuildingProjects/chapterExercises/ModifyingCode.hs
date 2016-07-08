732024c9922418a04eb0a9f01230515f60da6a2f
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-08 05:47:55 -0700

    here is a version with print statements at handleguess and gamover for numwrongs

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter13_BuildingProjects/chapterExercises/ModifyingCode.hs
+++
@@ -1,9 +0,0 @@
-import Control.Monad
-
-
-palindrome :: IO()
-palindrome = forever $ do
-    line <- getLine
-    case (line == reverse line) of
-        True -> putStrLn "YES"
-        False -> putStrLn "NO"
