50372aa826b99084e59760bbe9ffb7ad3e340a2f
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-07 23:17:03 -0700

    save

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter13_BuildingProjects/Hangman.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter13_BuildingProjects/Hangman.hs
@@ -17,7 +17,28 @@ filepath = "/datascience/projects/statisticallyfit/github/learningprogramming/" 
            "Haskell/HaskellTutorial/src/Books/ChrisAllen_HaskellFirstPrinciples/" ++
            "chapter13_BuildingProjects/data/dict.txt"
 
+
+minWordLength :: Int
+minWordLength = 5
+
+maxWordLength :: Int
+maxWordLength = 9
+
+
+
+
+-- note lines splits string at '\n'
+-- words splits words at '\n' AND ' '
 allWords :: IO WordList
 allWords = do
     dict <- readFile filepath
     return (lines dict)
+
+-- note filtering words by min and max length above
+gameWords :: IO WordList
+gameWords = do
+    aw <- allWords
+    return (filter gameLength aw)
+    where gameLength w =
+            let l = length (w :: String)
+            in l > minWordLength && l < maxWordLength 
