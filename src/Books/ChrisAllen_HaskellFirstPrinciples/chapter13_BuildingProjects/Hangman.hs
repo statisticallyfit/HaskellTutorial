40aea4133f1c466f8896bb93a8ccd343113403cc
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-08 01:54:59 -0700

    on handleGuess func

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter13_BuildingProjects/Hangman.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter13_BuildingProjects/Hangman.hs
@@ -3,7 +3,7 @@ module Hangman where
 import Control.Monad (forever)
 import Data.Char (toLower)
 import Data.Maybe (isJust)
-import Data.List (intersperse)
+import Data.List
 import System.Exit (exitSuccess)
 import System.Random (randomRIO)
 
@@ -41,4 +41,64 @@ gameWords = do
     return (filter gameLength aw)
     where gameLength w =
             let l = length (w :: String)
-            in l > minWordLength && l < maxWordLength 
+            in l > minWordLength && l < maxWordLength
+
+
+randomWord :: WordList -> IO String
+randomWord wordList = do
+    randomIndex <- randomRIO(0, (length wordList - 1))
+    return (wordList !! randomIndex)
+
+
+randomWord' :: IO String
+randomWord' = gameWords >>= randomWord
+
+
+
+
+------------------------------------------------------------------------------------
+data Puzzle = Puzzle String [Maybe Char] [Char]
+--                    [1]      [2]         [3]
+-- [1] = word we are trying to guess
+-- [2] = characters filled in so far
+-- [3] = all letters guessed so far
+
+{-
+
+instance Show Puzzle where
+    show (Puzzle _ discovered guessed) =
+        (intersperse ' ' $ fmap renderPuzzleChar discovered)
+        ++ " Guessed so far: " ++ guessed
+-}
+
+freshPuzzle :: String -> Puzzle
+freshPuzzle word = Puzzle word noneDiscovered []
+    where noneDiscovered = map (\_ -> Nothing) word
+        -- or could have used: [Nothing | _ <- word]
+
+
+
+-- note checks whether guessed char is element of the word.
+charInWord :: Puzzle -> Char -> Bool
+charInWord (Puzzle word _ _) g = elem g word
+
+alreadyGuessed :: Puzzle -> Char -> Bool
+alreadyGuessed (Puzzle _ _ guessed) g = elem g guessed
+
+
+renderPuzzleChar :: Maybe Char -> Char
+renderPuzzleChar (Just c) = c
+renderPuzzleChar Nothing = '_'
+
+-- note inserts correctly guessed char into the string
+-- uses zipper function which: if the guessChar equals the word char, then
+-- return the guessChar in a Just. Otherwise, just return the discovered char
+-- because it is either a Nothing or  Just (previously discovered char)
+-- note gc = guessedChar = charToAdd
+--      wc = wordChar (word letters one at a time)
+--      dc = discoveredChar (discovered maybes one at a time)
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle word discovered guesses) charToAdd =
+    Puzzle word newDiscovered (charToAdd : guesses)
+    where zipper gc wc dc = if wc == gc then Just wc else dc
+          newDiscovered = zipWith (zipper charToAdd) word discovered
