12513d07a77c5bd333ad8424da1ba037f54fa235
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-10 00:33:55 -0700

    save

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter11_AlgebraicDataTypes/chapterExercises/SamplePhone.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter11_AlgebraicDataTypes/chapterExercises/SamplePhone.hs
@@ -45,3 +45,31 @@ reversePhonePad (PhonePad buttons) = lookUpify buildList
 
 onSepLines :: Show a => [a] -> IO()
 onSepLines {-xs-} = putStr . concatMap ((++ "\n") . show) {-xs-}
+
+
+charToTaps :: PhonePad -> Char -> Maybe [(ButtonIdentifier, Presses)]
+charToTaps phonePad x = sequence $ map (flip lookup $ revPad) options
+  where
+    options = charToOption x
+    revPad = reversePhonePad phonePad
+
+
+stringToTaps :: PhonePad -> [Char] -> Maybe [(ButtonIdentifier, Presses)]
+stringToTaps phonepad x = fmap concat $ sequence $ map (charToTaps phonePad) x
+
+
+charToOption :: Char -> [Option]
+charToOption x
+  | isUpper x = Capitalize : Digit (toLower x) : []
+  | otherwise = Digit x : []
+
+convo :: [String]
+convo = ["Wanna play 20 questions",
+        "Ya",
+        "U 1st haha",
+        "Lol ok. Have u ever tasted alcohol lol",
+        "Lol ya",
+        "Wow ur cool haha. Ur turn",
+        "Ok. Do u think I am pretty Lol",
+        "Lol ya",
+        "Haha thanks just making sure rofl ur turn"]
