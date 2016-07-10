7888f0c119b9875a19d2298997b9e096858c1368
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-10 05:24:20 -0700

    trying to make toTaps func

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter11_AlgebraicDataTypes/chapterExercises/Phone.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter11_AlgebraicDataTypes/chapterExercises/Phone.hs
@@ -1,6 +1,9 @@
 module Phone where
 
 import Data.Char
+import Data.List
+import Data.Maybe
+
 
 {-
 IMPORTANT RULES:
@@ -12,16 +15,29 @@ more presses than digits in button get you back around to letters on button.
 
 
 type Symbol = Char
+type Digit = Char
 type Presses = Int -- >= 1
 data Operation = Number    -- 1,2,3,4,5,6,7,8,9,0
-               | Digit -- a,b,c,...z and A,B,C,...Z (only if capitalized)
-               | Capitalize -- the next one after capitalize will be default lowercase.
+               | LowerLetter -- a,b,c,...z and
+               | UpperLetter -- A,B,C,...Z (only if capitalized)
+               | Sign -- ?!.,
+               | NotCategorized -- not in phone : & % @ $
                deriving (Eq, Show)
 data Button = Button Operation Symbol deriving (Eq, Show)
 data Phone = PhonePad [Button] deriving (Eq, Show)
 
 
 
+phone :: Phone
+phone = PhonePad $ concat [numbers, lowLetters, uppLetters, signs]
+    where numbers = map (Button $ Number) (concatMap show [0..9])
+          lowLetters = map (Button $ LowerLetter) ['a' .. 'z']
+          uppLetters = map (Button $ UpperLetter) ['A' .. 'Z']
+          signs = map (Button $ Sign) "*^+ #,.?!" -- note has blank space for space.
+
+keyPad :: [String]
+keyPad = ["abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz",
+          "*^", "+0 ", "#.,?!"]
 
 text :: [String]
 text =
@@ -32,8 +48,6 @@ text =
      "Traced frozen lava scoured by winds.",
      "Glowing crystal caves."]
 
-
-
 displayPhone :: IO()
 displayPhone = putStr $ break ++ row123 ++ break ++ row456 ++ break ++
                    row789 ++ break ++ rowOp ++ break
@@ -41,35 +55,30 @@ displayPhone = putStr $ break ++ row123 ++ break ++ row456 ++ break ++
           row123 = "|   1       |   2 ABC   |   3 DEF   |\n"
           row456 = "|   4 GHI   |   5 JKL   |   6 MNO   |\n"
           row789 = "|   7 PQRS  |   8 TUV   |   9 WXYZ  |\n"
-          rowOp  = "|   * ^     |   0 + _   |   # .,    |\n"
+          rowOp  = "|   * ^     |   + 0 _   |   # .,?!  |\n"
 
+-- only expects the symbols present in the phonepad: letters,nums, *+-^#,.'?!
+toButton :: Symbol -> Button
+toButton n
+    | isLetter n && isLower n = Button LowerLetter n
+    | isLetter n && isUpper n = Button UpperLetter n
+    | isDigit n = Button Number n
+    | elem n "*^+ #,.?!" = Button Sign n
+    | otherwise = Button NotCategorized ' '
 
-
-{-
--- what does this do????
 -- note symbol can be 'a', 'A', '9', '*', '0'
-reverseTaps :: Phone -> Symbol -> [(Digit, Presses)]
-reverseTaps p sym
-    | isLetter sym =
-    | isDigit sym =
--}
+-- note called reverseTaps previously
+-- note press (*) to capitalize, and (+) to switch back and forth from nums to letters.
+-- postcondition: returns exception head if given char it doesn't know.
+toTaps :: Button -> [(Digit, Presses)]
+toTaps (Button Number n) = (n, 1)
+toTaps (Button Letter n) = (n, getTaps n)
+toTaps (Button Sign n) = classifySign n
+    where getTaps n = 1 + head $ catMaybes (map (elemIndex n) keyPad)
+          classifyNum n =
 
 
---phone :: Phone
-phone =
-    [('1', "1"),
-     ('2', "abc2"),
-     ('3', "def3"),
-     ('4', "ghi4"),
-     ('5', "jkl5"),
-     ('6', "mno6"),
-     ('7', "pqrs7"),
-     ('8', "tuv8"),
-     ('9', "wxyz9"),
-     ('*', "^"),
-     ('0', "_0"),
-     ('#', ".,")
-    ]
+
 
 
 {-
