1098f7bbb5ce57b68a251b7a6a7fa85d43e90512
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-15 01:04:52 -0700

    save

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter20_Foldable/chapterExercises/exercise20.5_Four.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter20_Foldable/chapterExercises/exercise20.5_Four.hs
@@ -3,3 +3,25 @@ import Data.Foldable
 import Data.Monoid
 
 
+
+data Four a b = Four a b b b deriving (Eq, Show)
+
+
+instance Foldable (Four a) where
+    foldMap f (Four a b c d) = f b <> f c <> f d
+
+
+
+
+main :: IO()
+main = do
+    print $ foldMap Sum (Four "a" 2 4 5)
+    print $ foldMap Sum (Four 1 2 8 9)
+    print $ fmap (foldr (-) (-8)) [Four "a" 3 3 2, Four "d" 1 6 5, Four "f" 7 1 (-9)]
+    print $ fmap (foldl (-) (-8)) [Four "a" 3 3 2, Four "d" 1 6 5, Four "f" 7 1 (-9)]
+    print $ foldMap Sum (Four 1 2 4 5)
+    print $ foldMap Any (Four 1 False True True)
+    print $ foldMap First (Four 0 (Nothing) (Just 100) (Just 5))
+    print $ foldMap Last (Four 0 (Just 44) (Just 100) (Just 23))
+    print $ foldMap Last (Four 0 (Just 23) (Just 100) Nothing)
+
