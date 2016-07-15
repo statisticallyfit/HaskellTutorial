5b9346123020138dd9a8029574565117e232785b
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-15 02:03:05 -0700

    done ex5 Four and figured out foldr and foldl implementation!

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter20_Foldable/chapterExercises/exercise20.5_Four.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter20_Foldable/chapterExercises/exercise20.5_Four.hs
@@ -8,6 +8,9 @@ data Four a b = Four a b b b deriving (Eq, Show)
 
 
 instance Foldable (Four a) where
+-- help how to write foldr and foldl?
+    foldr f z (Four a b c d) = (d `f` (c `f` (b `f` z)))
+    foldl f z (Four a b c d) = (((z `f` b) `f` c) `f` d)
     foldMap f (Four a b c d) = f b <> f c <> f d
 
 
@@ -17,6 +20,7 @@ main :: IO()
 main = do
     print $ foldMap Sum (Four "a" 2 4 5)
     print $ foldMap Sum (Four 1 2 8 9)
+    print $ foldr (+) 1 (Four 1 2 3 4)
     print $ fmap (foldr (-) (-8)) [Four "a" 3 3 2, Four "d" 1 6 5, Four "f" 7 1 (-9)]
     print $ fmap (foldl (-) (-8)) [Four "a" 3 3 2, Four "d" 1 6 5, Four "f" 7 1 (-9)]
     print $ foldMap Sum (Four 1 2 4 5)
