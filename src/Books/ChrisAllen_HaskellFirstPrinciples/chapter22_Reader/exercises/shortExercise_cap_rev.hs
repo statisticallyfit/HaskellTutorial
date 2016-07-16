1198e503365922f2056f1878ce37c084a645fd2a
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-16 11:29:29 -0700

    save on part 22.3

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter22_Reader/exercises/shortExercise_cap_rev.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter22_Reader/exercises/shortExercise_cap_rev.hs
@@ -19,12 +19,33 @@ fmapped = fmap rev cap
 tupledA :: [Char] -> ([Char], [Char])
 tupledA str = ((,) <$> rev <*> cap) str
 
+-- help help help todo: why doesn't it work to give argument right in here?
+-- help where is the Monad? How is tuple a monad... ?
+tupledD :: [Char] -> ([Char], [Char])
+tupledD = do
+    a <- rev
+    b <- cap
+    return (a, b)
 
-tupledM :: [Char] -> ([Char], [Char])
-tupledM str =
+
+-- (>>=) :: Monad m => m a -> (a -> m b) -> m b
+tupledB :: [Char] -> ([Char], [Char])
+tupledB = rev >>=
+            \revStr ->
+                cap >>=
+                    \capStr -> return (revStr, capStr)
+-- help help why doesn't this work:
+{-tupledB s = rev s >>=
+            \revStr ->
+                cap s >>=
+                    \capStr -> return (revStr, capStr)
+-}
+
 
 
 main = do
     print $ fmapped "Julie" == "EILUJ"
     print $ composed "Chris" == "SIRHC"
     print $ tupledA "Julie" == ("eiluJ","JULIE")
+    print $ tupledB "Julie" == ("eiluJ","JULIE")
+    print $ tupledD "Julie" == ("eiluJ","JULIE")
