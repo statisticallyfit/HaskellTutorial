11ef25aab94db28a4da8450b7b7dad54acc09c08
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-16 01:37:12 -0700

    save yay figured out tree foldr and foldlgit add --all! awesome

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter21_Traversable/chapterExercises/exercise_Tree.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter21_Traversable/chapterExercises/exercise_Tree.hs
@@ -9,9 +9,9 @@ data Tree a = Empty | Leaf a | Node (Tree a) a (Tree a)
     deriving (Eq, Show)
 
 
-t1 = Node (Leaf 4) 8 (Node (Leaf 7) 9 (Leaf 10))
-t2 = Node (Node (Node Empty 2 (Leaf 1)) 6 (Node (Leaf 3) 1 Empty)) 5 t1
-t3 = Node (Leaf 6) 2 (Leaf 4)
+t1 = Node (Leaf 1) 8 (Node (Leaf 7) 9 (Leaf 10))
+t2 = Node (Node (Node Empty 2 (Leaf 1)) 6 (Node (Leaf 8) 1 Empty)) 5 t1
+t3 = Node (Leaf 6) 2 (Leaf 5)
 t4 = Node (Leaf 1) 2 (Node (Leaf 3) 4 (Leaf 5))
 
 
@@ -24,9 +24,15 @@ instance Functor Tree where
 instance Foldable Tree where
     foldMap _ Empty = mempty
     foldMap f (Leaf a) = f a
-    foldMap f (Node left a right) = (foldMap f left) <> f a <> (foldMap f right)
+    foldMap f (Node left a right) = (foldMap f left) <> (f a) <> (foldMap f right)
 
-{-
+    foldl _ z Empty = z
+    foldl f z (Leaf a) = f z a
+    foldl f z (Node left a right) = foldl f (foldl f (f z a) left) right
+
     foldr _ z Empty = z
     foldr f z (Leaf a) = f a z
-    foldr f z (Node left a right) = foldr f (f a z) left-}
+    foldr f z (Node left a right) = foldr f (f a (foldr f z right)) left
+
+
+instance Traversable Tree where
