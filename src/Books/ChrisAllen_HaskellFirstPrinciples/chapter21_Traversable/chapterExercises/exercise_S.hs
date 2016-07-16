e6b0fcbd15a328365f999133ec7d83c2d2b8d47a
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-16 00:11:47 -0700

    save working on tree

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter21_Traversable/chapterExercises/exercise_S.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter21_Traversable/chapterExercises/exercise_S.hs
@@ -1,19 +1,39 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 import Test.QuickCheck
 import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
-
+import Data.Monoid
+import Data.Maybe
 
 
 data S n a = S (n a) a deriving (Eq, Show)
 
 
-instance Functor (S n) where
-    fmap f (S na a) = S na (f a)
+instance Functor n => Functor (S n) where
+    fmap f (S na a) = S (fmap f na) (f a)
 
 
-instance Foldable (S n) where
-    foldMap f (S na a) = f a
-{-
+instance Foldable n => Foldable (S n) where
+    foldMap f (S na a) = (foldMap f na) <> (f a)
+
 
 instance Traversable n => Traversable (S n) where
-    traverse f (S na a) = (S na) <$> f a-}
+    -- traverse :: (Applicative f, Traversable t) => (a -> f b) -> t a -> f (t b)
+    traverse f (S na a) = S <$> traverse f na <*> f a
+
+
+
+------------------------------------------------------------
+instance Arbitrary a => Arbitrary (S [] a) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        return (S [a] b)
+
+instance Eq a => EqProp (S [] a) where (=-=) = eq
+
+
+main = do
+    let trigger = undefined :: S [] (Int, String, [Int])
+    quickBatch (traversable trigger)
---
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter21_Traversable/chapterExercises/exercise_Tree.hs
@@ -0,0 +1,32 @@
+import Test.QuickCheck (Arbitrary, arbitrary, elements)
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+import Data.Monoid
+
+
+
+data Tree a = Empty | Leaf a | Node (Tree a) a (Tree a)
+    deriving (Eq, Show)
+
+
+t1 = Node (Leaf 4) 8 (Node (Leaf 7) 9 (Leaf 10))
+t2 = Node (Node (Node Empty 2 (Leaf 1)) 6 (Node (Leaf 3) 1 Empty)) 5 t1
+t3 = Node (Leaf 6) 2 (Leaf 4)
+t4 = Node (Leaf 1) 2 (Node (Leaf 3) 4 (Leaf 5))
+
+
+instance Functor Tree where
+    fmap _ Empty = Empty
+    fmap f (Leaf a) = Leaf (f a)
+    fmap f (Node left a right) = Node (fmap f left) (f a) (fmap f right)
+
+
+instance Foldable Tree where
+    foldMap _ Empty = mempty
+    foldMap f (Leaf a) = f a
+    foldMap f (Node left a right) = (foldMap f left) <> f a <> (foldMap f right)
+
+{-
+    foldr _ z Empty = z
+    foldr f z (Leaf a) = f a z
+    foldr f z (Node left a right) = foldr f (f a z) left-}
