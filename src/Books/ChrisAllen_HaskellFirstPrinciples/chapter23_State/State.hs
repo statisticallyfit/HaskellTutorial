7db74875a89448b538ec7fc24e2ed298a9a2cda3
BabitzdaBrave <amg.vintila@gmail.com>
2016-07-18 05:02:58 -0700

    example stdgen

--- src/Books/ChrisAllen_HaskellFirstPrinciples/chapter23_State/State.hs
+++ src/Books/ChrisAllen_HaskellFirstPrinciples/chapter23_State/State.hs
@@ -1,3 +1,4 @@
+import System.Random
 
 
 {-
@@ -6,5 +7,16 @@
 3. next :: g -> (Int, g) , where g is value of type StdGen.
 4. random :: (RandomGen g, Random a) => g -> (a, g)
 
+example
+
+Prelude> :t mkStdGen 0
+mkStdGen 0 :: StdGen
+Prelude> let sg = mkStdGen 0
+Prelude> :t next sg
+next sg :: (Int, StdGen)
+Prelude> next sg
+(2147482884,40014 40692)
+Prelude> next sg
+(2147482884,40014 40692)
 
 -}
