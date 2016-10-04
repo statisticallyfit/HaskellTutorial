45db608d506947d3551cdee48e88714946a8de8b
BabitzdaBrave <amg.vintila@gmail.com>
2016-10-04 08:01:48 -0700

    added more cases to crown func and findConsts

--- src/Projects/Tree/FormulaTree/ExprTreeOps.hs
+++ src/Projects/Tree/FormulaTree/ExprTreeOps.hs
@@ -61,6 +61,50 @@ instance Show Function where
     show (Ln e) = "ln(" ++ show e ++ ")"
     show (Log b a) = "log" ++ show b ++ "(" ++ show a ++ ")"
 
+{-
+
+e3 should look like:
+(-)
+ |
+ `-- (+)
+ |    |
+ |    `-- (*)
+ |    |    |
+ |    |    `-- 4
+ |    |    `-- x
+ |    |
+ |    `-- (*)
+ |         |
+ |         `-- 3
+ |         `-- (^)
+ |              |
+ |              `-- x
+ |              `-- 5
+ |
+ `-- sin(*) ...
+
+instance Show a => Show (Tree a) where
+    show theTree = draw 1 "\n     " "\n" "\n" theTree
+        where
+        draw _ _ _ _ Empty = "_"
+        draw _ _ _ _ (Leaf n) = show n
+        draw c i lb rb (Node op left right)
+            | isNode left && isNode right
+                = "(" ++ op ++ ")" ++ lb' ++ draw c' i' lb' rb' left ++
+                    lb' ++ draw c' i' lb' rb' right
+            | isNode left
+                = "(" ++ op ++ ")" ++ lb' ++ draw c' i' lb' rb' left ++
+                    rb' ++ draw c' i' lb' rb' right
+            | isNode right
+                = rb' ++ draw c' i' lb' rb' left ++
+                    "(" ++ op ++ ")" ++ lb' ++ draw c' i' lb' rb' right
+            where
+            i' = i ++ "     "
+            c' = c + 1
+            lb' = lb ++ " |\n `-- "
+            rb' = " |\n"  ++ i'
+-}
+
 
 instance Show a => Show (Tree a) where
     show tree = draw 1 "\n" tree
@@ -73,6 +117,7 @@ instance Show a => Show (Tree a) where
             where
             indent' = indent ++ "    "
             count' = count + 1
+
 
 
 infixl 6 .+
@@ -96,8 +141,13 @@ e2 = e1 .+ Num(2) .* X .+ Num(7) .* X
 e3 = Num(4) .* X .+ Num(3) .* X .^ Num(5) .- (F (Sin (Num(3) .+ X)))
 e4 = Neg(F (Sin X)) .* Neg(Num(2))
 e5 = Neg (Num(4) .* X .+ Num(2) .* Y)
+
 -- TODO test percolate minus cases
-e6 = Node "-" (Node "-" (Leaf $ Num 1) (Leaf $ Num 4)) (Leaf $ Num (-10))
+t1 :: Tree Expr
+t1 = Node "-" (Node "-" (Leaf $ Num 1) (Leaf $ Num 4)) (Leaf $ Num (-10))
+t2 = Node "-" Empty (Node "*" (Leaf X) (Leaf $ Num 3))
+t3 = Node "-" (Leaf $ Num 4) Empty
+
 
 {-
 NOTE:
@@ -111,6 +161,14 @@ leftSub :: Tree a -> Tree a
 leftSub (Node _ left _) = left
 leftSub _ = error "leftSub"
 
+rightSub :: Tree a -> Tree a
+rightSub (Node _ _ right) = right
+rightSub _ = error "rightSub"
+
+isNode :: Tree a -> Bool
+isNode (Node _ _ _) = True
+isNode _ = False
+
 isLeaf :: Tree a -> Bool
 isLeaf (Leaf x) = True
 isLeaf _ = False
@@ -119,10 +177,17 @@ leafVal :: Tree a -> a
 leafVal (Leaf x) = x
 leafVal _ = error "leafVal"
 
+treeOp :: Tree a -> String
+treeOp (Node op _ _) = op
+treeOp _ = error "treeOp"
+
 isEmpty :: Tree a -> Bool
 isEmpty Empty = True
 isEmpty _ = False
 
+isThree :: Tree a -> Bool
+isThree (Node n (Leaf x) (Leaf y)) = True
+isThree _ = False
 
 
 mapTree :: (Int -> Int) -> Tree Expr -> Tree Expr
@@ -137,7 +202,7 @@ mkTree :: Expr -> Tree Expr
 mkTree X = Leaf X
 mkTree Y = Leaf Y
 mkTree (Num n) = Leaf (Num n)
-mkTree (F func) = Leaf (F func)
+mkTree (F func) = Leaf (F func) -- TODO should I show expressions in functino as a tree as well?
 mkTree (Neg (Num n)) = Leaf (Num (-n))
 mkTree (Neg e) = Node "-" Empty (mkTree e)
 mkTree (Add e1 e2) = Node "+" (mkTree e1) (mkTree e2)
@@ -167,17 +232,105 @@ getExpr (Node "^" left right) = Pow (getExpr left) (getExpr right)
 
 -- if we found a constant, percolate the rest, if they exist...
 percolate :: Tree Expr -> Tree Expr
-percolate (Node op Empty right) = percolate right
+percolate (Node op (Leaf s) Empty) = Leaf s
+percolate node@(Node op Empty (Leaf s)) = node
+percolate (Node op Empty right) = Node op Empty (percolate right) -- might just be neg so we need struct
 percolate (Node op left Empty) = percolate left
-percolate (Node "+" left right) = Node "+" (perc (0 +) left) (perc (0 +) right)
-percolate (Node "-" left right) = Node "-" (perc gSub left) (perc gSub right)
+percolate (Node op left right)
+    | op == "+" = perc (0 +) $ Node "+" left right
+    | op == "-" = perc gSub $ Node "-" left right
+    | op == "*" = perc (1 *) $ Node "*" left right
+    | op == "/" = perc gDiv $ Node "/" left right
+    | op == "^" = perc gPow $ Node "^" left right
     where gSub = \x -> x - 0
-percolate (Node "*" left right) = Node "*" (perc (1 *) left) (perc (1 *) right)
-percolate (Node "/" left right) = Node "/" (perc gDiv left) (perc gDiv right)
-    where gDiv = \x -> x `div` 1
-percolate (Node "^" left right) = Node "^" (perc gPow left) (perc gPow  right)
-    where gPow = \x -> x ^ 1
-{-
+          gDiv = \x -> x `div` 1
+          gPow = \x -> x ^ 1
+
+
+
+-- assume no constants in the right subtree, just on the left, always, assume we have
+-- already passed this tree into the rearrange const function
+-- TODO need to fix this to avoid cases where op is lower precedence than function
+-- example: op = + and we plus over leaf connected to a "*"
+
+perc :: (Int -> Int) -> Tree Expr -> Tree Expr
+-- note: the num-num cases
+perc f (Node op (Leaf (Num n)) (Leaf (Num m)))
+    | op == "+" = Leaf (Num ((f n) + m))
+    | op == "-" = Leaf (Num ((f n) - m))
+    | op == "*" = Leaf (Num ((f n) * m))
+    | op == "/" = Leaf (Num ((f n) `div` m))
+    | op == "^" = Leaf (Num ((f n) ^ m))
+perc f (Node op (Leaf (Num n)) leaf@(Leaf varOrFunc))
+    | op == "+" = Node "+" (Leaf (Num (f n))) leaf
+    | op == "-" = Node "-" (Leaf (Num (f n))) leaf
+    | op == "*" = Node "*" (Leaf (Num (f n))) leaf
+    | op == "/" = Node "/" (Leaf (Num (f n))) leaf
+    | op == "^" = Node "^" (Leaf (Num (f n))) leaf
+perc f (Node op leaf@(Leaf varOrFunc) (Leaf (Num m)))
+    | op == "+" = Node "+" leaf (Leaf (Num (f m)))
+    | op == "-" = Node "-" leaf (Leaf (Num (f m)))
+    | op == "*" = Node "*" leaf (Leaf (Num (f m)))
+    | op == "/" = Node "/" leaf (Leaf (Num (f m)))
+    | op == "^" = Node "^" leaf (Leaf (Num (f m)))
+perc f (Node op v1@(Leaf varOrFunc1) v2@(Leaf varOrFunc2))
+    | op == "+" = Leaf (Num (f 0)) --Node op (Node op Empty (Leaf (Num (f 0)))) (Node op v1 v2)
+    | op == "-" = Leaf (Num (f 0))
+    | op == "*" = Leaf (Num (f 1))
+    | op == "/" = Leaf (Num (f 1))
+    | op == "^" = Leaf (Num (f 1))
+perc f (Node op (Leaf (Num n)) right)
+    | op == "+" = Node "+" Empty (perc ((f n) +) right)
+    | op == "-" = Node "-" Empty (perc ((f n) -) right)
+    | op == "*" = Node "*" Empty (perc ((f n) *) right)
+    | op == "/" = Node "/" Empty (perc ((f n) `div`) right)
+    | op == "^" = Node "^" Empty (perc ((f n) ^) right)
+perc f (Node op left (Leaf (Num m)))
+    | op == "+" = Node "+" (perc (\x -> x + (f m)) left) Empty
+    | op == "-" = Node "-" (perc (\x -> x - (f m)) left) Empty
+    | op == "*" = Node "*" (perc (\x -> x * (f m)) left) Empty
+    | op == "/" = Node "/" (perc (\x -> x `div` (f m)) left) Empty
+    | op == "^" = Node "^" (perc (\x -> x ^ (f m)) left) Empty
+perc f (Node op leaf@(Leaf varOrFunc) right)
+    | op == "+" = Node "+" leaf (perc f right)
+    | op == "-" = Node "-" leaf (perc f right)
+    | op == "*" = Node "*" leaf (perc f right)
+    | op == "/" = Node "/" leaf (perc f right)
+    | op == "^" = Node "^" leaf (perc f right)
+perc f (Node op left leaf@(Leaf varOrFunc))
+    | op == "+" = Node "+" (perc f left) leaf
+    | op == "-" = Node "-" (perc f left) leaf
+    | op == "*" = Node "*" (perc f left) leaf
+    | op == "/" = Node "/" (perc f left) leaf
+    | op == "^" = Node "^" (perc f left) leaf
+
+perc f (Node op Empty right) = Node op Empty (perc f right)
+perc f (Node op left Empty) = perc f left
+-- note: the node-node cases
+perc f (Node op left right) = Node op (perc f left) (perc f right)
+{-perc f (Node op (Leaf (Num n)) leaf@(Leaf varOrFunc)) = Node op (Leaf (Num (f n))) leaf
+perc f (Node op leaf@(Leaf varOrFunc) (Leaf (Num m))) = Node op leaf (Leaf (Num (f m)))
+perc f (Node op (Leaf (Num n)) right) = Node op Empty (perc g right)
+    where (g, _) = opFunc op f n
+perc f (Node op left (Leaf (Num m))) = Node op (perc h left) Empty
+    where (_, h) = opFunc op f m
+perc f (Node op leaf@(Leaf varOrFunc) right) = Node op leaf (perc f right)
+perc f (Node op left leaf@(Leaf varOrFunc)) = Node op (perc f left) leaf-}
+
+
+-- perc f (Node _ _ _) = Leaf (Num (f 1))
+
+
+opFunc :: String -> (Int -> Int) -> Int -> ((Int -> Int), (Int -> Int))
+opFunc op f n
+    | op == "+" = (((f n) +), (\x -> x + (f n)))
+    | op == "-" = (((f n) -), (\x -> x - (f n)))
+    | op == "*" = (((f n) *), (\x -> x * (f n)))
+    | op == "/" = (((f n) `div`), (\x -> x `div` (f n)))
+    | op == "^" = (((f n) ^), (\x -> x ^ (f n)))
+
+
+{- NOTe all together (same times together)
 percolate (Node "+" (Leaf (Num n)) (Leaf (Num m))) = Leaf $ Num $ n + m
 percolate (Node "+" (Leaf (Num n)) right) = Node "+" Empty (perc (n +) right)
 percolate (Node "+" left (Leaf (Num n))) = Node "+" (perc (n +) left) Empty
@@ -208,45 +361,6 @@ percolate (Node "^" left (Leaf (Num n))) = Node "^" Empty (perc (\x -> x ^ n) le
 percolate (Node "^" leaf@(Leaf varOrFunc) right) = Node "^" leaf (perc id right)
 percolate (Node "^" left right) = Node "^" (percolate left) (percolate right)
 -}
-
-
-
--- assume no constants in the right subtree, just on the left, always, assume we have
--- already passed this tree into the rearrange const function
-perc :: (Int -> Int) -> Tree Expr -> Tree Expr
--- note: the num-num cases
-perc f (Node "+" (Leaf (Num n)) (Leaf (Num m))) = Leaf (Num (n + m))
-perc f (Node "-" (Leaf (Num n)) (Leaf (Num m))) = Leaf (Num (n - m))
-perc f (Node "*" (Leaf (Num n)) (Leaf (Num m))) = Leaf (Num ((f n) * m))
-perc f (Node "/" (Leaf (Num n)) (Leaf (Num m))) = Leaf (Num (n `div` m)) -- TODO make fraction
-perc f (Node "^" (Leaf (Num n)) (Leaf (Num m))) = Leaf (Num (n ^ m))
--- note: the var/func - var/func cases
-perc f (Node "+" (Leaf varOrFunc1) (Leaf varOrFunc2)) = Leaf (Num (f 0))
-perc f (Node "-" (Leaf varOrFunc1) (Leaf varOrFunc2)) = Leaf (Num (f 0)) -- TODO check if correct
-perc f (Node "*" (Leaf varOrFunc1) (Leaf varOrFunc2)) = Leaf (Num (f 1))
-perc f (Node "/" (Leaf varOrFunc1) (Leaf varOrFunc2)) = Leaf (Num (f 1)) -- TODO check correctness
-perc f (Node "^" (Leaf varOrFunc1) (Leaf varOrFunc2)) = Leaf (Num (f 1)) -- TODO check correctness
-
-perc f (Node op (Leaf (Num n)) leaf@(Leaf varOrFunc)) = Node op (Leaf (Num (f n))) leaf
-perc f (Node op leaf@(Leaf varOrFunc) (Leaf (Num m))) = Node op leaf (Leaf (Num (f m)))
-perc f (Node op (Leaf (Num n)) right) = Node op Empty (perc g right)
-    where (g, _) = opFunc op f n
-perc f (Node op left (Leaf (Num m))) = Node op (perc h left) Empty
-    where (_, h) = opFunc op f m
-perc f (Node op leaf@(Leaf varOrFunc) right) = Node op leaf (perc f right)
-perc f (Node op left leaf@(Leaf varOrFunc)) = Node op (perc f left) leaf
-
-perc f (Node op Empty right) = Node op Empty (perc f right)
-perc f (Node op left Empty) = Node op (perc f left) Empty
--- note: the node-node cases
-perc f (Node op left right) = Node op (perc f left) (perc f right)
-
-opFunc op func nn
-    | op == "+" = (((func nn) +), (\x -> x + (func nn)))
-    | op == "-" = (((func nn) -), (\x -> x - (func nn)))
-    | op == "*" = (((func nn) *), (\x -> x * (func nn)))
-    | op == "/" = (((func nn) `div`), (\x -> x `div` (func nn)))
-    | op == "^" = (((func nn) ^), (\x -> x ^ (func nn)))
 
 {-
 -- note: the meat add cases
@@ -287,7 +401,7 @@ perc f (Node "^" left leaf@(Leaf varOrFunc)) = Node "^" (perc f left) leaf
 -}
 
 
--- perc f (Node _ _ _) = Leaf (Num (f 1))
+
 
 
 -- TODO idea: make array holding coefficients of powers
@@ -316,6 +430,7 @@ squash (Node op leaf@(Leaf l) right) = Node op leaf (squash right)
 -- NOTE this is awesomely cool function ! Test thoroughly to make sure not missing any cases.
 remove :: Tree Expr -> Tree Expr -> Tree Expr
 remove t Empty = Empty
+remove t (Leaf s) = if t == (Leaf s) then Empty else Leaf s
 remove t node@(Node _ l1@(Leaf x) l2@(Leaf y))
     | l1 == t = l2
     | l2 == t = l1
@@ -332,30 +447,51 @@ remove t node@(Node op left right)
     | otherwise = Node op (remove t left) (remove t right)
 
 
+-- findConst returns the first constant we find and puts Empty in its place.
+-- (after percolate, it is at bottom)
+findConst :: Tree Expr -> Maybe Int
+findConst Empty = Nothing
+findConst (Leaf (Num n)) = Just n
+findConst (Leaf varOrFunc) = Nothing
+findConst (Node op (Leaf (Num n)) (Leaf (Num m)))
+    = error "findConst: not simplified! n, m on same branch"
+findConst (Node op (Leaf (Num n)) leaf@(Leaf varOrFunc)) = Just n
+findConst (Node op leaf@(Leaf varOrFunc) (Leaf (Num n)) ) = Just n
+findConst (Node op (Leaf l1) (Leaf l2)) = Nothing
+findConst (Node op (Leaf l) right) = findConst right
+findConst (Node op left (Leaf l)) = findConst left
+findConst (Node op left right) = error "help" -- TODO what to do?
+
+
+-- puts the constant given in the first available place (right under first node)
+crownTree :: Maybe String -> Int -> Tree Expr -> Tree Expr
+crownTree opMaybe n tree
+    | isNothing opMaybe = crown' n tree
+    | otherwise = Node (fromJust opMaybe) (Leaf (Num n)) tree
+    where
+    crown' n Empty = Empty
+    crown' n (Leaf (Num m)) = Leaf (Num $ n + m) --- HELP todo
+    crown' n leaf@(Leaf varOrFunc) = Node "+" (Leaf (Num n)) leaf
+    crown' n tree@(Node op left right) = Node op (Leaf (Num n)) tree
+
 
 -- assume should be no unsimplified constants like Node num num left (passed to perc first)
 -- assume no Empty , so has been passed to squash first.
 rearrangeConsts :: Tree Expr -> Tree Expr
+rearrangeConsts Empty = Empty
+rearrangeConsts (Leaf s) = Leaf s
 rearrangeConsts tree
+    | isLeaf tree' = tree'
     | isNothing constMaybe = Empty
-    | otherwise = crownTree num tree''
+    -- | isLeaf tree'' = crownTree opMaybe num tree''
+    | otherwise = crownTree opMaybe num tree''
     where tree' = remove Empty $ percolate tree
           constMaybe = findConst tree'
           num = fromJust constMaybe
-          tree'' = remove (Leaf (Num num)) tree'
+          (tree'', opMaybe) = if isThree tree'
+                         then (remove (Leaf (Num num)) tree', Just $ treeOp tree')
+                         else (remove (Leaf (Num num)) tree', Nothing)
 
-
--- findConst returns the first constant we find and puts Empty in its place.
--- (after percolate, it is at bottom)
-findConst :: Tree Expr -> Maybe Int
-findConst (Node "*" (Leaf (Num n)) leaf@(Leaf varOrFunc)) = Just n
-findConst (Node "*" (Leaf l1) (Leaf l2)) = Nothing
-findConst (Node "*" (Leaf l) right) = findConst right
-
-
--- puts the constant given in the first available place (right under first node)
-crownTree :: Int -> Tree Expr -> Tree Expr
-crownTree n tree@(Node op left right) = Node op (Leaf (Num n)) tree
 
 
 
