6d35db59a07d5e96adc3398381e27efb931d8f86
BabitzdaBrave <amg.vintila@gmail.com>
2016-10-08 10:34:08 -0700

    monomial separation of show is not working

--- src/Projects/Tree/FormulaTree/ExprTreeOps.hs
+++ src/Projects/Tree/FormulaTree/ExprTreeOps.hs
@@ -93,82 +93,164 @@ instance Show Expr where
         where
         show' X = "x"
         show' Y = "y"
-        show' (Num n) = show' n
-        show' (F func) = show' func
+        show' (Num n) = show n
+        show' (F func) = show func
         show' (Add e1 e2) = show' e1 ++ " + " ++ show' e2
         show' (Sub e1 e2) = show' e1 ++ " - " ++ show' e2
 
-        show' (Mul (Num n) (Num m)) = "(" ++ show' n ++ ")(" ++ show' m ++ ")"
-        show' (Mul (Num n) p@(Pow _ _)) = show' n ++ show p
-        show' (Mul (Num n) X) = show' n ++ show' X
-        show' (Mul (Neg (Num n)) X) = "-" ++ show' n ++ show' X
-        show' (Neg (Mul (Num n) X)) = "-" ++ show' n ++ show' X
-        show' (Mul (Num n) rest) = show' n ++ "(" ++ show' rest ++ ")"
+        show' (Mul (Num n) (Num m)) = "(" ++ show n ++ ")(" ++ show m ++ ")"
+        show' (Mul (Num n) p@(Pow _ _)) = show n ++ show' p
+        show' (Mul (Num n) X) = show n ++ show' X
+        show' (Mul (Neg (Num n)) X) = "-" ++ show n ++ show' X
+        show' (Neg (Mul (Num n) X)) = "-" ++ show n ++ show' X
+        show' (Mul (Num n) rest) = show n ++ "(" ++ show' rest ++ ")"
 
-        show (Mul (Mul (Mul (Mul rest p1@(Pow _ _)) p2@(Pow _ _)) p3@(Pow _ _)) p4@(Pow _ _))
+        show' (Mul (Mul (Mul (Mul rest p1@(Pow _ _)) p2@(Pow _ _)) p3@(Pow _ _)) p4@(Pow _ _))
             = if isPow rest
-            then "(" ++ show rest ++ ")(" ++ show p1 ++ ")(" ++ show p2 ++ ")(" ++ show p3 ++ ")(" ++ show p4 ++ ")"
-            else show rest ++ "(" ++ show p1 ++ ")(" ++ show p2 ++ ")(" ++ show p3 ++ ")(" ++ show p4 ++ ")"
-        show (Mul (Mul (Mul rest p1@(Pow _ _)) p2@(Pow _ _)) p3@(Pow _ _))
+            then "(" ++ show' rest ++ ")(" ++ show' p1 ++ ")(" ++ show' p2 ++ ")(" ++ show' p3 ++ ")(" ++ show' p4 ++ ")"
+            else show' rest ++ "(" ++ show' p1 ++ ")(" ++ show' p2 ++ ")(" ++ show' p3 ++ ")(" ++ show' p4 ++ ")"
+        show' (Mul (Mul (Mul rest p1@(Pow _ _)) p2@(Pow _ _)) p3@(Pow _ _))
             = if isPow rest
-            then "(" ++ show rest ++ ")(" ++ show p1 ++ ")(" ++ show p2 ++ ")(" ++ show p3 ++ ")"
-            else show rest ++ "(" ++ show p1 ++ ")(" ++ show p2 ++ ")(" ++ show p3 ++ ")"
-        show (Mul (Mul rest p1@(Pow _ _)) p2@(Pow _ _))
+            then "(" ++ show' rest ++ ")(" ++ show' p1 ++ ")(" ++ show' p2 ++ ")(" ++ show' p3 ++ ")"
+            else show' rest ++ "(" ++ show' p1 ++ ")(" ++ show' p2 ++ ")(" ++ show' p3 ++ ")"
+        show' (Mul (Mul rest p1@(Pow _ _)) p2@(Pow _ _))
             = if isPow rest
-            then "(" ++ show rest ++ ")(" ++ show p1 ++ ")(" ++ show p2 ++ ")"
-            else show rest ++ "(" ++ show p1 ++ ")(" ++ show p2 ++ ")"
-        show (Mul rest p@(Pow _ _))
+            then "(" ++ show' rest ++ ")(" ++ show' p1 ++ ")(" ++ show' p2 ++ ")"
+            else show' rest ++ "(" ++ show' p1 ++ ")(" ++ show' p2 ++ ")"
+        show' (Mul rest p@(Pow _ _))
             = if isPow rest
-            then "(" ++ show rest ++ ")(" ++ show p ++ ")"
-            else show rest ++ "(" ++ show p ++ ")"
+            then "(" ++ show' rest ++ ")(" ++ show' p ++ ")"
+            else show' rest ++ "(" ++ show' p ++ ")"
 
 
-        show (Mul (Mul (Mul (Mul rest (Num n1)) (Num n2)) (Num n3)) (Num n4))
+        show' (Mul (Mul (Mul (Mul rest (Num n1)) (Num n2)) (Num n3)) (Num n4))
             = if isNum rest
             then "(" ++ show rest ++ ")(" ++ show n1 ++ ")(" ++ show n2 ++ ")(" ++ show n3 ++ ")(" ++ show n4 ++ ")"
-            else show rest ++ "(" ++ show n1 ++ ")(" ++ show n2 ++ ")(" ++ show n3 ++ ")(" ++ show n4 ++ ")"
-        show (Mul (Mul (Mul rest (Num n1)) (Num n2)) (Num n3))
+            else show' rest ++ "(" ++ show n1 ++ ")(" ++ show n2 ++ ")(" ++ show n3 ++ ")(" ++ show n4 ++ ")"
+        show' (Mul (Mul (Mul rest (Num n1)) (Num n2)) (Num n3))
             = if isNum rest
             then "(" ++ show rest ++ ")(" ++ show n1 ++ ")(" ++ show n2 ++ ")(" ++ show n3 ++ ")"
-            else show rest ++ "(" ++ show n1 ++ ")(" ++ show n2 ++ ")(" ++ show n3 ++ ")"
-        show (Mul (Mul rest (Num n1)) (Num n2))
+            else show' rest ++ "(" ++ show n1 ++ ")(" ++ show n2 ++ ")(" ++ show n3 ++ ")"
+        show' (Mul (Mul rest (Num n1)) (Num n2))
             = if isNum rest
             then "(" ++ show rest ++ ")(" ++ show n1 ++ ")(" ++ show n2 ++ ")"
-            else show rest ++ "(" ++ show n1 ++ ")(" ++ show n2 ++ ")"
-        show (Mul rest (Num n))
+            else show' rest ++ "(" ++ show n1 ++ ")(" ++ show n2 ++ ")"
+        show' (Mul rest (Num n))
             = if isNum rest
             then "(" ++ show rest ++ ")(" ++ show n ++ ")"
-            else show rest ++ "(" ++ show n ++ ")"
+            else show' rest ++ "(" ++ show n ++ ")"
 
-        show (Mul d1@(Div _ _) d2@(Div _ _)) = "(" ++ show d1 ++ ") (" ++ show d2 ++ ")"
-        show (Mul d@(Div _ _) m@(Mul _ _)) = "(" ++ show d ++ ") (" ++ show m ++ ")"
-        show (Mul m@(Mul _ _) d@(Div _ _)) = "(" ++ show m ++ ") (" ++ show d ++ ")"
-        show (Mul m1@(Mul (Num a) (Pow _ _)) m2@(Mul (Num b) (Pow _ _)))
-            = "(" ++ show m1 ++ ") (" ++ show m2 ++ ")"
-        show (Mul e1 a@(Add _ _)) = show e1 ++ "(" ++ show a ++ ")"
-        show (Mul e1 ng@(Neg (Num n))) = show e1 ++ "(" ++ show ng ++ ")"
-        show (Mul e1 e2)
+        show' (Mul d1@(Div _ _) d2@(Div _ _)) = "(" ++ show' d1 ++ ") (" ++ show' d2 ++ ")"
+        show' (Mul d@(Div _ _) m@(Mul _ _)) = "(" ++ show' d ++ ") (" ++ show' m ++ ")"
+        show' (Mul m@(Mul _ _) d@(Div _ _)) = "(" ++ show' m ++ ") (" ++ show' d ++ ")"
+        show' (Mul m1@(Mul (Num a) (Pow _ _)) m2@(Mul (Num b) (Pow _ _)))
+            = "(" ++ show' m1 ++ ") (" ++ show' m2 ++ ")"
+        show' (Mul e1 a@(Add _ _)) = show' e1 ++ "(" ++ show' a ++ ")"
+        show' (Mul e1 ng@(Neg (Num n))) = show' e1 ++ "(" ++ show' ng ++ ")"
+        show' (Mul e1 e2)
             = if isPow e1 && isPow e2
-             then show e1 ++ " * " ++ show e2
+             then show' e1 ++ " * " ++ show' e2
              else if isPow e1 && isNum e2
-             then "(" ++ show e1 ++ ")(" ++ show e2 ++ ")"
-             else show e1 ++ show e2
+             then "(" ++ show' e1 ++ ")(" ++ show' e2 ++ ")"
+             else show' e1 ++ show' e2
 
-        show (Div (Num n) (Num m)) = show n ++ "/" ++ show m
-        show (Div e1 e2) = "(" ++ show e1 ++ ") / (" ++ show e2 ++ ")"
-        show (Pow x d@(Div (Num a) (Num b))) = show x ++ "^(" ++ show d ++ ")"
-        show (Pow x d@(Div e1 e2)) = show x ++ "^" ++ show d
-        show (Pow e1 e2) = show e1 ++ "^" ++ show e2
+        show' (Div (Num n) (Num m)) = show n ++ "/" ++ show m
+        show' (Div e1 e2) = "(" ++ show' e1 ++ ") / (" ++ show' e2 ++ ")"
+        show' (Pow x d@(Div (Num a) (Num b))) = show' x ++ "^(" ++ show' d ++ ")"
+        show' (Pow x d@(Div e1 e2)) = show' x ++ "^" ++ show' d
+        show' (Pow e1 e2) = show' e1 ++ "^" ++ show' e2
 
-        show (Neg m@(Mul _ _)) = "-" ++ show m
-        show (Neg d@(Div _ _)) = "-" ++ show d
-        show (Neg p@(Pow _ _)) = "-" ++ show p
-        show (Neg f@(F _)) = "-" ++ show f
-        show (Neg X) = "-" ++ show X
-        show (Neg Y) = "-" ++ show Y
-        show (Neg e) = "-(" ++ show e ++ ")"
+        show' (Neg m@(Mul _ _)) = "-" ++ show' m
+        show' (Neg d@(Div _ _)) = "-" ++ show' d
+        show' (Neg p@(Pow _ _)) = "-" ++ show' p
+        show' (Neg f@(F _)) = "-" ++ show' f
+        show' (Neg X) = "-" ++ show' X
+        show' (Neg Y) = "-" ++ show' Y
+        show' (Neg e) = "-(" ++ show' e ++ ")"
+
+-- NOTE original
+{-
+instance Show Expr where
+    show X = "x"
+    show Y = "y"
+    show (Num n) = show n
+    show (F func) = show func
+    show (Add e1 e2) = show e1 ++ " + " ++ show e2
+    show (Sub e1 e2) = show e1 ++ " - " ++ show e2
+
+    show (Mul (Num n) (Num m)) = "(" ++ show n ++ ")(" ++ show m ++ ")"
+    show (Mul (Num n) p@(Pow _ _)) = show n ++ show p
+    show (Mul (Num n) X) = show n ++ show X
+    show (Mul (Neg (Num n)) X) = "-" ++ show n ++ show X
+    show (Neg (Mul (Num n) X)) = "-" ++ show n ++ show X
+    show (Mul (Num n) Y) = show n ++ show Y
+    show (Mul (Neg (Num n)) Y) = "-" ++ show n ++ show Y
+    show (Neg (Mul (Num n) Y)) = "-" ++ show n ++ show Y
+    show (Mul (Num n) rest) = show n ++ "(" ++ show rest ++ ")"
+
+    show (Mul (Mul (Mul (Mul rest p1@(Pow _ _)) p2@(Pow _ _)) p3@(Pow _ _)) p4@(Pow _ _))
+        = if isPow rest
+        then "(" ++ show rest ++ ")(" ++ show p1 ++ ")(" ++ show p2 ++ ")(" ++ show p3 ++ ")(" ++ show p4 ++ ")"
+        else show rest ++ "(" ++ show p1 ++ ")(" ++ show p2 ++ ")(" ++ show p3 ++ ")(" ++ show p4 ++ ")"
+    show (Mul (Mul (Mul rest p1@(Pow _ _)) p2@(Pow _ _)) p3@(Pow _ _))
+        = if isPow rest
+        then "(" ++ show rest ++ ")(" ++ show p1 ++ ")(" ++ show p2 ++ ")(" ++ show p3 ++ ")"
+        else show rest ++ "(" ++ show p1 ++ ")(" ++ show p2 ++ ")(" ++ show p3 ++ ")"
+    show (Mul (Mul rest p1@(Pow _ _)) p2@(Pow _ _))
+        = if isPow rest
+        then "(" ++ show rest ++ ")(" ++ show p1 ++ ")(" ++ show p2 ++ ")"
+        else show rest ++ "(" ++ show p1 ++ ")(" ++ show p2 ++ ")"
+    show (Mul rest p@(Pow _ _))
+        = if isPow rest
+        then "(" ++ show rest ++ ")(" ++ show p ++ ")"
+        else show rest ++ "(" ++ show p ++ ")"
 
 
+    show (Mul (Mul (Mul (Mul rest (Num n1)) (Num n2)) (Num n3)) (Num n4))
+        = if isNum rest
+        then "(" ++ show rest ++ ")(" ++ show n1 ++ ")(" ++ show n2 ++ ")(" ++ show n3 ++ ")(" ++ show n4 ++ ")"
+        else show rest ++ "(" ++ show n1 ++ ")(" ++ show n2 ++ ")(" ++ show n3 ++ ")(" ++ show n4 ++ ")"
+    show (Mul (Mul (Mul rest (Num n1)) (Num n2)) (Num n3))
+        = if isNum rest
+        then "(" ++ show rest ++ ")(" ++ show n1 ++ ")(" ++ show n2 ++ ")(" ++ show n3 ++ ")"
+        else show rest ++ "(" ++ show n1 ++ ")(" ++ show n2 ++ ")(" ++ show n3 ++ ")"
+    show (Mul (Mul rest (Num n1)) (Num n2))
+        = if isNum rest
+        then "(" ++ show rest ++ ")(" ++ show n1 ++ ")(" ++ show n2 ++ ")"
+        else show rest ++ "(" ++ show n1 ++ ")(" ++ show n2 ++ ")"
+    show (Mul rest (Num n))
+        = if isNum rest
+        then "(" ++ show rest ++ ")(" ++ show n ++ ")"
+        else show rest ++ "(" ++ show n ++ ")"
+
+    show (Mul d1@(Div _ _) d2@(Div _ _)) = "(" ++ show d1 ++ ") (" ++ show d2 ++ ")"
+    show (Mul d@(Div _ _) m@(Mul _ _)) = "(" ++ show d ++ ") (" ++ show m ++ ")"
+    show (Mul m@(Mul _ _) d@(Div _ _)) = "(" ++ show m ++ ") (" ++ show d ++ ")"
+    show (Mul m1@(Mul (Num a) (Pow _ _)) m2@(Mul (Num b) (Pow _ _)))
+        = "(" ++ show m1 ++ ") (" ++ show m2 ++ ")"
+    show (Mul e1 a@(Add _ _)) = show e1 ++ "(" ++ show a ++ ")"
+    show (Mul e1 ng@(Neg (Num n))) = show e1 ++ "(" ++ show ng ++ ")"
+    show (Mul e1 e2)
+        = if isPow e1 && isPow e2
+         then show e1 ++ " * " ++ show e2
+         else if isPow e1 && isNum e2
+         then "(" ++ show e1 ++ ")(" ++ show e2 ++ ")"
+         else show e1 ++ show e2
+
+    show (Div (Num n) (Num m)) = show n ++ "/" ++ show m
+    show (Div e1 e2) = "(" ++ show e1 ++ ") / (" ++ show e2 ++ ")"
+    show (Pow x d@(Div (Num a) (Num b))) = show x ++ "^(" ++ show d ++ ")"
+    show (Pow x d@(Div e1 e2)) = show x ++ "^" ++ show d
+    show (Pow e1 e2) = show e1 ++ "^" ++ show e2
+
+    show (Neg m@(Mul _ _)) = "-" ++ show m
+    show (Neg d@(Div _ _)) = "-" ++ show d
+    show (Neg p@(Pow _ _)) = "-" ++ show p
+    show (Neg f@(F _)) = "-" ++ show f
+    show (Neg X) = "-" ++ show X
+    show (Neg Y) = "-" ++ show Y
+    show (Neg e) = "-(" ++ show e ++ ")"
+-}
 
 
 -- TODO do show instance for odd and even:
